2c498ea0fadec1dfcd7600aaa0c57a4cb0a3563f
Xtung <yougame29@gmail.com>
2024-11-13 17:55:10 +0700

    danhmucsp

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,29 @@
 <?php 
-
+session_start();
 // Require file Common
 require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
 require_once 'controllers/DashboardController.php';
+require_once 'controllers/DanhMucController.php';
 
 // Require toàn bộ file Models
-
+require_once "models/DanhMucModel.php";
 // Route
 $act = $_GET['act'] ?? '/';
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
-match ($act) {
+match ($act){
     // Dashboards
     '/'                 => (new DashboardController())->index(),
+    // quản lý danh mục sản phẩm
+    'danh-mucs'         => (new DanhMucController())->index(),
+    'form-add-danh-muc' => (new DanhMucController())->create(),
+    'them-danh-muc'     => (new DanhMucController())->store(),
+    'form-sua-danh-muc'     => (new DanhMucController())->edit(),
+    'sua-danh-muc'      => (new DanhMucController())->update(),
+    'xoa-danh-muc'      => (new DanhMucController())->destroy()
+
 };
--- commons/function.php
+++ commons/function.php
@@ -3,9 +3,9 @@
 // Kết nối CSDL qua PDO
 function connectDB() {
     // Kết nối CSDL
-    $host = DB_HOST;
-    $port = DB_PORT;
-    $dbname = DB_NAME;
+    $host = "localhost";
+    $port = "root";
+    $dbname = "thucphamchucnang";
 
     try {
         $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", DB_USERNAME, DB_PASSWORD);
