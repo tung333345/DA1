5e1cfa2ef4a5b6e5c9e4bfe02c014e402c1f189e
chithin <nchithin28@gmail.com>
2024-11-23 11:15:41 +0700

    new

--- commons/env.php
+++ commons/env.php
@@ -7,7 +7,7 @@ define('BASE_URL'   , 'http://localhost/base_du_an_1/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
-define('DB_NAME'    , '');  // Tên database
+// define('DB_NAME'     , ''); // Tên database
 define('DB_NAME'    , 'thucphamchucnang');  // Tên database
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
